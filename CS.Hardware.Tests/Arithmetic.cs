892242396f5ad558eabcd061633b503563c3d7eb
larsrotgers <larsrotgers@gmail.com>
2017-05-20 14:11:17 +0200

    commit

--- CS.Hardware.Tests/Arithmetic.cs
+++ CS.Hardware.Tests/Arithmetic.cs
@@ -90,9 +90,372 @@ namespace CS.Hardware.Tests
         }
 
         [TestMethod]
-        public void ALU_1()
+        public void ALU_fxy_0()
         {
-            
+            var size = 16;
+            var x = new bool[size];
+            var y = new bool[size];
+            for (int i = 0; i < size; i++)
+            {
+                x[i] = true;
+                y[i] = true;
+            }
+            var alu = new ALU(x, y, zx: true, zy: true, f: true);
+            for (int i = 0; i < size; i++)
+            {
+                Assert.IsFalse(alu.Out[i]);
+            }
+            Assert.IsTrue(alu.ZR);
+        }
+
+        [TestMethod]
+        public void ALU_fxy_1()
+        {
+            var size = 16;
+            var x = new bool[size];
+            var y = new bool[size];
+            var alu = new ALU(x, y, zx: true, nx: true, zy: true, ny: true, f: true, no: true);
+            for (int i = 0; i < size; i++)
+            {
+                if (i == 0)
+                {
+                    Assert.IsTrue(alu.Out[i]);
+                }
+                else
+                {
+                    Assert.IsFalse(alu.Out[i]);
+                }
+            }
+            Assert.IsFalse(alu.ZR);
+        }
+
+        [TestMethod]
+        public void ALU_fxy_neg1()
+        {
+            var size = 16;
+            var x = new bool[size];
+            var y = new bool[size];
+            for (int i = 0; i < size; i++)
+            {
+                x[i] = true;
+                y[i] = true;
+            }
+            var alu = new ALU(x, y, zx: true, nx: true, zy: true, f: true);
+            for (int i = 0; i < size; i++)
+            {
+                Assert.IsTrue(alu.Out[i]);
+            }
+            Assert.IsTrue(alu.NG);
+            Assert.IsFalse(alu.ZR);
+        }
+
+        [TestMethod]
+        public void ALU_fxy_x()
+        {
+            var size = 16;
+            var x = new bool[size];
+            var y = new bool[size];
+            for (int i = 0; i < size; i++)
+            {
+                x[i] = true;
+            }
+            x[size - 1] = false;
+            var alu = new ALU(x, y, zy: true, ny: true);
+            for (int i = 0; i < size - 1; i++)
+            {
+                Assert.IsTrue(alu.Out[i]);
+            }
+            Assert.IsFalse(x[size - 1]);
+            Assert.IsFalse(alu.NG);
+            Assert.IsFalse(alu.ZR);
+        }
+
+        [TestMethod]
+        public void ALU_fxy_y()
+        {
+            var size = 16;
+            var x = new bool[size];
+            var y = new bool[size];
+            for (int i = 0; i < size; i++)
+            {
+                y[i] = true;
+            }
+            y[size - 1] = false;
+            var alu = new ALU(x, y, zx: true, nx: true);
+            for (int i = 0; i < size - 1; i++)
+            {
+                Assert.IsTrue(alu.Out[i]);
+            }
+            Assert.IsFalse(y[size - 1]);
+            Assert.IsFalse(alu.NG);
+            Assert.IsFalse(alu.ZR);
+        }
+
+        [TestMethod]
+        public void ALU_fxy_notx()
+        {
+            var size = 16;
+            var x = new bool[size];
+            var y = new bool[size];
+            for (int i = 0; i < size; i++)
+            {
+                x[i] = true;
+            }
+            var alu = new ALU(x, y, zy: true, ny: true, no: true);
+            for (int i = 0; i < size; i++)
+            {
+                Assert.IsFalse(alu.Out[i]);
+            }
+        }
+
+        [TestMethod]
+        public void ALU_fxy_noty()
+        {
+            var size = 16;
+            var x = new bool[size];
+            var y = new bool[size];
+            for (int i = 0; i < size; i++)
+            {
+                y[i] = true;
+            }
+            var alu = new ALU(x, y, zx: true, nx: true, no: true);
+            for (int i = 0; i < size; i++)
+            {
+                Assert.IsFalse(alu.Out[i]);
+            }
+        }
+
+        [TestMethod]
+        public void ALU_fxy_negx()
+        {
+            var size = 16;
+            var x = new bool[size];
+            var y = new bool[size];
+            x[0] = true;
+            var alu = new ALU(x, y, zy: true, ny: true, f: true, no: true);
+            for (int i = 0; i < size; i++)
+            {
+                Assert.IsTrue(alu.Out[i]);
+            }
+            Assert.IsTrue(alu.NG);
+        }
+
+        [TestMethod]
+        public void ALU_fxy_negy()
+        {
+            var size = 16;
+            var x = new bool[size];
+            var y = new bool[size];
+            y[0] = true;
+            var alu = new ALU(x, y, zx: true, zy: true, f: true, no: true);
+            for (int i = 0; i < size; i++)
+            {
+                Assert.IsTrue(alu.Out[i]);
+            }
+            Assert.IsTrue(alu.NG);
+        }
+
+        [TestMethod]
+        public void ALU_fxy_xadd1()
+        {
+            var size = 16;
+            var x = new bool[size];
+            var y = new bool[size];
+            x[0] = true;
+            var alu = new ALU(x, y, nx: true, zy: true, ny: true, f: true, no: true);
+            for (int i = 0; i < size; i++)
+            {
+                if (i == 1)
+                {
+                    Assert.IsTrue(alu.Out[i]);
+                }
+                else
+                {
+                    Assert.IsFalse(alu.Out[i]);
+                }
+            }
+            Assert.IsFalse(alu.NG);
+            Assert.IsFalse(alu.ZR);
+        }
+
+        [TestMethod]
+        public void ALU_fxy_yadd1()
+        {
+            var size = 16;
+            var x = new bool[size];
+            var y = new bool[size];
+            y[0] = true;
+            var alu = new ALU(x, y, nx: true, zx: true, ny: true, f: true, no: true);
+            for (int i = 0; i < size; i++)
+            {
+                if (i == 1)
+                {
+                    Assert.IsTrue(alu.Out[i]);
+                }
+                else
+                {
+                    Assert.IsFalse(alu.Out[i]);
+                }
+            }
+            Assert.IsFalse(alu.NG);
+            Assert.IsFalse(alu.ZR);
+        }
+
+        [TestMethod]
+        public void ALU_fxy_xsub1()
+        {
+            var size = 16;
+            var x = new bool[size];
+            var y = new bool[size];
+            x[1] = true;
+            var alu = new ALU(x, y, zy: true, ny: true, f: true);
+            for (int i = 0; i < size; i++)
+            {
+                if (i == 0)
+                {
+                    Assert.IsTrue(alu.Out[i]);
+                }
+                else
+                {
+                    Assert.IsFalse(alu.Out[i]);
+                }
+            }
+            Assert.IsFalse(alu.NG);
+            Assert.IsFalse(alu.ZR);
+        }
+
+        [TestMethod]
+        public void ALU_fxy_ysub1()
+        {
+            var size = 16;
+            var x = new bool[size];
+            var y = new bool[size];
+            y[1] = true;
+            var alu = new ALU(x, y, zx: true, nx: true, f: true);
+            for (int i = 0; i < size; i++)
+            {
+                if (i == 0)
+                {
+                    Assert.IsTrue(alu.Out[i]);
+                }
+                else
+                {
+                    Assert.IsFalse(alu.Out[i]);
+                }
+            }
+            Assert.IsFalse(alu.NG);
+            Assert.IsFalse(alu.ZR);
+        }
+
+        [TestMethod]
+        public void ALU_fxy_xaddy()
+        {
+            var size = 16;
+            var x = new bool[size];
+            var y = new bool[size];
+            x[0] = true;
+            y[1] = true;
+            var alu = new ALU(x, y, f: true);
+            for (int i = 0; i < size; i++)
+            {
+                if (i == 0 || i == 1)
+                {
+                    Assert.IsTrue(alu.Out[i]);
+                }
+                else
+                {
+                    Assert.IsFalse(alu.Out[i]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ALU_fxy_xsuby()
+        {
+            var size = 16;
+            var x = new bool[size];
+            var y = new bool[size];
+            x[0] = true;
+            x[1] = true;
+            y[0] = true;
+            var alu = new ALU(x, y, nx: true, f: true, no: true);
+            for (int i = 0; i < size; i++)
+            {
+                if (i == 1)
+                {
+                    Assert.IsTrue(alu.Out[i]);
+                }
+                else
+                {
+                    Assert.IsFalse(alu.Out[i]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ALU_fxy_ysubx()
+        {
+            var size = 16;
+            var x = new bool[size];
+            var y = new bool[size];
+            y[0] = true;
+            y[1] = true;
+            x[0] = true;
+            var alu = new ALU(x, y, ny: true, f: true, no: true);
+            for (int i = 0; i < size; i++)
+            {
+                if (i == 1)
+                {
+                    Assert.IsTrue(alu.Out[i]);
+                }
+                else
+                {
+                    Assert.IsFalse(alu.Out[i]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ALU_fxy_xandy()
+        {
+            var size = 16;
+            var x = new bool[size];
+            var y = new bool[size];
+            x[5] = true;
+            y[5] = true;
+            var alu = new ALU(x, y);
+            for (int i = 0; i < size; i++)
+            {
+                if (i == 5)
+                {
+                    Assert.IsTrue(alu.Out[i]);
+                }
+                else
+                {
+                    Assert.IsFalse(alu.Out[i]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ALU_fxy_xory()
+        {
+            var size = 16;
+            var x = new bool[size];
+            var y = new bool[size];
+            x[5] = true;
+            var alu = new ALU(x, y, nx: true, ny: true, no: true);
+            for (int i = 0; i < size; i++)
+            {
+                if (i == 5)
+                {
+                    Assert.IsTrue(alu.Out[i]);
+                }
+                else
+                {
+                    Assert.IsFalse(alu.Out[i]);
+                }
+            }
         }
     }
 }
--- CS.Hardware/BooleanArithmetic/ALU.cs
+++ CS.Hardware/BooleanArithmetic/ALU.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CS.Hardware.BooleanLogic.Gates;
+using CS.Hardware.BooleanLogic.Multiplexors;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -12,7 +14,6 @@ namespace CS.Hardware.BooleanArithmetic
         /// 16-bit data input.
         /// </summary>
         public bool[] X { get; set; } = new bool[SIZE];
-
         /// <summary>
         /// 16-bit data input.
         /// </summary>
@@ -51,7 +52,11 @@ namespace CS.Hardware.BooleanArithmetic
         /// <summary>
         /// 16-bit data output.
         /// </summary>
-        public bool[] Out { get; set; } = new bool[SIZE];
+        public bool[] Out
+        {
+            get { return _out; }
+        }
+        private bool[] _out { get; set; } = new bool[SIZE];
 
         /// <summary>
         /// True iff out=0
@@ -75,7 +80,7 @@ namespace CS.Hardware.BooleanArithmetic
         /// <param name="ny">Negate the y input</param>
         /// <param name="f">Function code: 1 for Add, 0 for And</param>
         /// <param name="no">Negate output</param>
-        public ALU(bool[] x, bool[] y, bool zx, bool nx, bool zy, bool ny, bool f, bool no)
+        public ALU(bool[] x, bool[] y, bool zx = false, bool nx = false, bool zy = false, bool ny = false, bool f = false, bool no = false)
         {
             X = x;
             Y = y;
@@ -88,9 +93,40 @@ namespace CS.Hardware.BooleanArithmetic
             Execute();
         }
 
-        private void Execute()
+        public void Execute()
         {
+            // Input stream of 16-bit for x
+            var zeroX = new And16(X, new bool[SIZE]);
+            var dmuxZx = new Mux16(X, zeroX.Out, ZX);
+            var negateX = new Not16(dmuxZx.Out);
+            var dmuxNx = new Mux16(dmuxZx.Out, negateX.Out, NX);
 
+            // Input stream of 16-bit for y
+            var zeroY = new And16(Y, new bool[SIZE]);
+            var dmuxZy = new Mux16(Y, zeroY.Out, ZY);
+            var negateY = new Not16(dmuxZy.Out);
+            var dmuxNy = new Mux16(dmuxZy.Out, negateY.Out, NY);
+
+            // Add/And arithmetic
+            var adder = new Adder16(dmuxNx.Out, dmuxNy.Out);
+            var anding = new And16(dmuxNx.Out, dmuxNy.Out);
+            var f = new Mux16(anding.Out, adder.Out, F);
+
+            // Negating output
+            var negateOut = new Not16(f.Out);
+            var outDmux = new Mux16(f.Out, negateOut.Out, NO);
+
+            var result = outDmux.Out;
+
+            // 16-bit eq. comparison
+            var or8way = new Or8Way(result);
+            var eq = new Not(or8way.Out);
+            ZR = eq.Out;
+
+            // 16-bit neg. comparison
+            NG = result[SIZE - 1];
+
+            _out = result;
         }
     }
 }
